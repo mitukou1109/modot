64fde4126737825bff0d4e636bf05b2963d8def2
mitukou1109 <mitukou1109@gmail.com>
2023-12-04 03:47:08 +0900

    Add nodes to launch

--- modot_recognition/launch/recognition_bringup.launch.py
+++ modot_recognition/launch/recognition_bringup.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -30,9 +31,31 @@ def generate_launch_description():
         )
     )
 
+    obstacle_detector_node = Node(
+        package="modot_recognition",
+        executable="obstacle_detector",
+        name="obstacle_detector",
+        output="screen",
+        remappings=[("point_cloud", "/realsense/depth/color/points")],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=[
+            "-d",
+            PathJoinSubstitution(
+                [modot_recognition_share_dir, "rviz", "recognition.rviz"]
+            ),
+        ],
+    )
+
     return LaunchDescription(
         [
             realsense2_camera_launch,
-            vidvipo_yolov2_tiny_launch,
+            # vidvipo_yolov2_tiny_launch,
+            obstacle_detector_node,
+            rviz_node,
         ]
     )
