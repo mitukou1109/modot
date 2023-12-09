258780cad9bcf233fc2cc503e79d03ee44317378
mitukou1109 <mitukou1109@gmail.com>
2023-12-09 23:11:39 +0900

    Implement obstacle detection

--- modot_recognition/include/modot_recognition/obstacle_detector.hpp
+++ modot_recognition/include/modot_recognition/obstacle_detector.hpp
@@ -5,6 +5,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 #include "modot_lib/parameter_updater.hpp"
 
@@ -18,19 +20,26 @@ private:
 
   void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
-  static double getMeanZ(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
-                         const pcl::PointIndices::ConstPtr& indices);
+  static double getMeanZ(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud, const pcl::PointIndices& indices);
+
+  static double getMin2DDistance(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
+                                 const pcl::PointIndices& indices);
 
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_pub_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_sub_;
 
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+
   std::shared_ptr<rclcpp::ParameterEventHandler> parameter_event_handler_;
   std::unique_ptr<modot_lib::ParameterUpdater> parameter_updater_;
 
+  std::string global_frame_;
   double leaf_size_;
   double sac_threshold_;
   double plane_segmentation_ratio_;
   double cluster_tolerance_;
   uint32_t min_cluster_size_;
   uint32_t max_cluster_size_;
+  double obstacle_range_;
 };
--- modot_recognition/src/obstacle_detector.cpp
+++ modot_recognition/src/obstacle_detector.cpp
@@ -5,28 +5,35 @@
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.hpp>
 
 using namespace std::placeholders;
 
-const std::vector<std::array<uint8_t, 3>> ObstacleDetector::PALETTE = { { { 255, 0, 0 } },   { { 0, 255, 0 } },
-                                                                        { { 0, 0, 255 } },   { { 255, 255, 0 } },
-                                                                        { { 255, 0, 255 } }, { { 0, 255, 255 } } };
+const std::vector<std::array<uint8_t, 3>> ObstacleDetector::PALETTE = { { { 0, 255, 0 } },   { { 0, 0, 255 } },
+                                                                        { { 255, 255, 0 } }, { { 255, 0, 255 } },
+                                                                        { { 0, 255, 255 } }, { { 255, 255, 255 } } };
 
 ObstacleDetector::ObstacleDetector()
   : Node("obstacle_detector")
+  , global_frame_("world")
   , leaf_size_(0.05)
   , sac_threshold_(0.03)
   , plane_segmentation_ratio_(0.3)
   , cluster_tolerance_(0.1)
   , min_cluster_size_(10)
   , max_cluster_size_(10000)
+  , obstacle_range_(2.0)
 {
+  this->declare_parameter("global_frame", global_frame_);
   this->declare_parameter("leaf_size", leaf_size_);
   this->declare_parameter("sac_threshold", sac_threshold_);
   this->declare_parameter("plane_segmentation_ratio", plane_segmentation_ratio_);
   this->declare_parameter("cluster_tolerance", cluster_tolerance_);
   this->declare_parameter("min_cluster_size", static_cast<int>(min_cluster_size_));
   this->declare_parameter("max_cluster_size", static_cast<int>(max_cluster_size_));
+  this->declare_parameter("obstacle_range", obstacle_range_);
+
+  this->get_parameter("global_frame", global_frame_);
 
   parameter_event_handler_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
   parameter_updater_ = std::make_unique<modot_lib::ParameterUpdater>(parameter_event_handler_);
@@ -36,12 +43,14 @@ ObstacleDetector::ObstacleDetector()
   parameter_updater_->addParameter("cluster_tolerance", cluster_tolerance_);
   parameter_updater_->addParameter("min_cluster_size", min_cluster_size_);
   parameter_updater_->addParameter("max_cluster_size", max_cluster_size_);
+  parameter_updater_->addParameter("obstacle_range", obstacle_range_);
 
   point_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("~/point_cloud", 10);
   point_cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
       "point_cloud", 10, std::bind(&ObstacleDetector::pointCloudCallback, this, _1));
 
-
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 }
 
 void ObstacleDetector::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
@@ -49,29 +58,26 @@ void ObstacleDetector::pointCloudCallback(const sensor_msgs::msg::PointCloud2::S
   auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
   pcl::fromROSMsg(*msg, *cloud);
 
-  if (cloud->width > 0)
+  if (cloud->width >= 3)
   {
     pcl::VoxelGrid<pcl::PointXYZRGB> voxel_grid;
     voxel_grid.setInputCloud(cloud);
     voxel_grid.setLeafSize(leaf_size_, leaf_size_, leaf_size_);
     voxel_grid.filter(*cloud);
-  }
 
-  if (cloud->width >= 3)
-  {
     pcl::SACSegmentation<pcl::PointXYZRGB> seg;
     seg.setOptimizeCoefficients(true);
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
     seg.setDistanceThreshold(sac_threshold_);
 
-    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-    extract.setNegative(true);
-
-    auto planes = std::vector<pcl::PointIndices::Ptr>();
+    std::vector<pcl::PointIndices::Ptr> planes;
     auto coefficients = std::make_shared<pcl::ModelCoefficients>();
     auto segmented_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
     pcl::copyPointCloud(*cloud, *segmented_cloud);
+
+    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+    extract.setNegative(true);
 
     while (segmented_cloud->width >= plane_segmentation_ratio_ * cloud->width)
     {
@@ -87,40 +93,82 @@ void ObstacleDetector::pointCloudCallback(const sensor_msgs::msg::PointCloud2::S
 
     if (planes.size() > 0)
     {
-      auto ground_plane = *std::max_element(planes.begin(), planes.end(),
-                                            [cloud](const pcl::PointIndices::Ptr& a, const pcl::PointIndices::Ptr& b) {
-                                              return getMeanZ(cloud, a) < getMeanZ(cloud, b);
-                                            });
+      const auto& ground_plane = *std::max_element(
+          planes.begin(), planes.end(), [cloud](const pcl::PointIndices::Ptr& a, const pcl::PointIndices::Ptr& b) {
+            return getMeanZ(cloud, *a) < getMeanZ(cloud, *b);
+          });
       extract.setInputCloud(cloud);
       extract.setIndices(ground_plane);
       extract.filter(*cloud);
     }
-  }
 
-  if (cloud->width >= min_cluster_size_)
-  {
-    auto tree = std::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>();
-    tree->setInputCloud(cloud);
-
-    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ece;
-    std::vector<pcl::PointIndices> clusters;
-    ece.setClusterTolerance(cluster_tolerance_);
-    ece.setMinClusterSize(min_cluster_size_);
-    ece.setMaxClusterSize(max_cluster_size_);
-    ece.setSearchMethod(tree);
-    ece.setInputCloud(cloud);
-    ece.extract(clusters);
-
-    for (auto itr = clusters.cbegin(); itr != clusters.cend(); ++itr)
+    if (cloud->width >= min_cluster_size_)
     {
-      auto color = PALETTE[std::distance(clusters.cbegin(), itr) % PALETTE.size()];
-      for (auto& index : itr->indices)
+      auto tree = std::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>();
+      tree->setInputCloud(cloud);
+
+      pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ece;
+      std::vector<pcl::PointIndices> clusters;
+      ece.setClusterTolerance(cluster_tolerance_);
+      ece.setMinClusterSize(min_cluster_size_);
+      ece.setMaxClusterSize(max_cluster_size_);
+      ece.setSearchMethod(tree);
+      ece.setInputCloud(cloud);
+      ece.extract(clusters);
+
+      if (clusters.size() > 0)
       {
-        cloud->points[index].r = color[0];
-        cloud->points[index].g = color[1];
-        cloud->points[index].b = color[2];
+        for (auto itr = clusters.cbegin(); itr != clusters.cend(); ++itr)
+        {
+          const auto& color = PALETTE[std::distance(clusters.cbegin(), itr) % PALETTE.size()];
+          for (const auto& index : itr->indices)
+          {
+            cloud->points[index].r = color[0];
+            cloud->points[index].g = color[1];
+            cloud->points[index].b = color[2];
+          }
+        }
+
+        if (pcl_ros::transformPointCloud(global_frame_, msg->header.stamp, *cloud, msg->header.frame_id, *cloud,
+                                         *tf_buffer_))
+        {
+          cloud->header.frame_id = global_frame_;
+
+          auto closest_obstacle = *std::min_element(clusters.begin(), clusters.end(),
+                                                    [cloud](const pcl::PointIndices& a, const pcl::PointIndices& b) {
+                                                      return getMin2DDistance(cloud, a) < getMin2DDistance(cloud, b);
+                                                    });
+
+          for (const auto& index : closest_obstacle.indices)
+          {
+            cloud->points[index].r = 255;
+            cloud->points[index].g = 0;
+            cloud->points[index].b = 0;
+          }
+
+          if (getMin2DDistance(cloud, closest_obstacle) < obstacle_range_)
+          {
+            RCLCPP_INFO_STREAM(this->get_logger(), "Obstacle detected!");
+          }
+
+          auto cluster_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
+          pcl::copyPointCloud(*cloud, clusters, *cluster_cloud);
+          cloud = cluster_cloud;
+        }
+      }
+      else
+      {
+        cloud->clear();
       }
     }
+    else
+    {
+      cloud->clear();
+    }
+  }
+  else
+  {
+    cloud->clear();
   }
 
   sensor_msgs::msg::PointCloud2 cloud_msg;
@@ -129,12 +177,19 @@ void ObstacleDetector::pointCloudCallback(const sensor_msgs::msg::PointCloud2::S
 }
 
 double ObstacleDetector::getMeanZ(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
-                                  const pcl::PointIndices::ConstPtr& indices)
+                                  const pcl::PointIndices& indices)
 {
-  double mean_z = 0;
-  for (auto& index : indices->indices)
-  {
-    mean_z += cloud->points[index].z;
-  }
-  return mean_z / indices->indices.size();
+  return std::accumulate(indices.indices.begin(), indices.indices.end(), 0.0,
+                         [cloud](double sum, const std::size_t& index) { return sum + cloud->points[index].z; }) /
+         indices.indices.size();
+}
+
+double ObstacleDetector::getMin2DDistance(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
+                                          const pcl::PointIndices& indices)
+{
+  auto closest_index = *std::min_element(
+      indices.indices.begin(), indices.indices.end(), [cloud](const std::size_t& a, const std::size_t& b) {
+        return std::hypot(cloud->points[a].x, cloud->points[a].y) < std::hypot(cloud->points[b].x, cloud->points[b].y);
+      });
+  return std::hypot(cloud->points[closest_index].x, cloud->points[closest_index].y);
 }
