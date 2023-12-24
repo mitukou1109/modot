ca1439c968fa949f6700bfc0423aa082b8cb16bb
mitukou1109 <mitukou1109@gmail.com>
2023-12-24 12:56:30 +0900

    Revert "Use rmw_fastrtps"
    This reverts commit 90f96704f58a6f0447fc19626e193e750a257a98.

--- modot_recognition/launch/vidvipo_yolov2_tiny.launch.py
+++ modot_recognition/launch/vidvipo_yolov2_tiny.launch.py
@@ -1,6 +1,6 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, GroupAction
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 
@@ -23,23 +23,6 @@ def generate_launch_description():
         [darknet_ros_config_dir, "vidvipo-yolov2-tiny.yaml"]
     )
 
-    rmw_fastrtps_group = GroupAction(
-        [
-            SetEnvironmentVariable(
-                "FASTRTPS_DEFAULT_PROFILES_FILE",
-                PathJoinSubstitution(
-                    [
-                        modot_recognition_share_dir,
-                        "config",
-                        "DEFAULT_FASTRTPS_PROFILES.xml",
-                    ]
-                ),
-            ),
-            SetEnvironmentVariable("RMW_FASTRTPS_USE_QOS_FROM_XML", "1"),
-            SetEnvironmentVariable("RMW_IMPLEMENTATION", "rmw_fastrtps_cpp"),
-        ]
-    )
-
     darknet_ros_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
@@ -56,7 +39,6 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            rmw_fastrtps_group,
             darknet_ros_launch,
         ]
     )
