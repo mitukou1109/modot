5482bc55a6e7f785418fdcae7c704a2cd3611551
mitukou1109 <mitukou1109@gmail.com>
2024-05-11 23:10:37 +0900

    add necessary installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 [ -d src ] || { echo "$(pwd)/src not found, run this script in workspace root"; exit 1; }
 
+sudo apt install python3-vcstool python3-pip python3-colcon-common-extensions ffmpeg ffmpeg-python
+
 vcs import src --input https://raw.githubusercontent.com/mitukou1109/modot/master/modot.repos
 
+rosdep update
 rosdep install -iyr --from-paths src
 
+pip install -r src/modot_notification/requirements.txt
 pip install -r src/face_recognition_ros/requirements.txt
 pip install -r src/ultralytics_ros/requirements.txt
 
